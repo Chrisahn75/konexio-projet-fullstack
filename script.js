e247a93032237fc9b3786232acc85fa76df0985b
Chrisahn75 <chris.ahona@gmail.com>
2022-01-19 16:33:35 +0100

    debut

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-function getAllCountries(){
+$("#btnShowData").click(function getAllCountries() {
+    $.ajax({
+        url: "https://restcountries.com/v3.1/all",
+        success: function (data) {
+            data.forEach((country) => {
+                $("#country").html(`<p>${country.name.common}</p>`);
+                $("#capital").html(`<p>${country.capital}</p>`);
+                $("#continent").html(`<p>${country.continents}</p>`);
+                for(currency in country.currencies)  {
+                    $("#monnaie").html(`<p>${country.currencies[currency].name}</p>`);
+                }
+                for(language in country.languages)  {
+                    $("#languages").html(`<p>${country.languages[language]}</p>`);
+                }
     
-}
+    
+            })
+           
+        }
+    })
+});
+
+getAllCountries()
