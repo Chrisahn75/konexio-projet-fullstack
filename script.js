a7c8badcbf498c9e982c0a04006412492b97d107
Chrisahn75 <chris.ahona@gmail.com>
2022-01-20 10:57:26 +0100

    form part

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+$("#btnShowData").click(function () {
+    let country = $("input").val();
+    let search = $('input[id=search]:checked', '#searchPart').val()
+
+    if (country) {
+        if () {
+            
+// LIST OF ALL 
 $("#btnShowData").click(function getAllCountries() {
     $.ajax({
         url: "https://restcountries.com/v3.1/all",
@@ -6,9 +14,11 @@ $("#btnShowData").click(function getAllCountries() {
                 $("#list").append(`<h3>${country.name.common}</h3>`);
                 $("#list").append(`<li> Capitale : ${country.capital}</li>`);
                 $("#list").append(`<li> Continent: ${country.continents}</li>`);
+
                 for(currency in country.currencies)  {
                     $("#list").append(`<li> Monnaie : ${country.currencies[currency].name}</li>`);
                 }
+
                 for(language in country.languages)  {
                     $("#list").append(`<li> Langue : ${country.languages[language]}</li>`);
                 }
