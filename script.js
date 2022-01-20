eb35e43c4fac904701f21a0a2d35330b5e8e02dc
Chrisahn75 <chris.ahona@gmail.com>
2022-01-20 12:26:05 +0100

    form part

--- script.js
+++ script.js
@@ -1,10 +1,54 @@
+// FORM PART
 $("#btnShowData").click(function () {
     let country = $("input").val();
-    let search = $('input[id=search]:checked', '#searchPart').val()
+    let search = $("input[id=search]:checked", "#searchPart").val()
 
     if (country) {
-        if () {
-            
+        if (search === 1 || !search) {
+            $.ajax({
+                url: `https://restcountries.com/v3.1/name/${country}`,
+                success: function (data) {
+                    data.forEach((country) => {
+                        $("#list").append(`<li> Country: ${country.name.common}</li>`);
+                        $("#list").append(`<li> Capital: ${country.capital}</li>`);
+                        $("#list").append(`<li> Continent: ${country.continents}</li>`);
+
+                        for(currency in country.currencies)  {
+                            $("#list").append(`<li> Monnaie : ${country.currencies[currency].name}</li>`);
+                        }
+
+                        for(language in country.languages)  {
+                            $("#list").append(`<li> Langue : ${country.languages[language]}</li>`);
+                        }
+                    })
+                },
+             });
+        } else {
+            $.ajax({
+                url: `https://restcountries.com/v3.1/capital/${country}`,
+                success: function (data) {
+                    data.forEach((country) => {
+                        $("#list").append(`<li> Country: ${country.name.common}</li>`);
+                        $("#list").append(`<li> Capital: ${country.capital}</li>`);
+                        $("#list").append(`<li> Continent: ${country.continents}</li>`);
+
+                        for(currency in country.currencies)  {
+                            $("#list").append(`<li> Monnaie : ${country.currencies[currency].name}</li>`);
+                        }
+
+                        for(language in country.languages)  {
+                            $("#list").append(`<li> Langue : ${country.languages[language]}</li>`);
+                        }
+                    })
+                },
+            });
+        };
+    } else {
+        getAllCountries();
+    }
+});
+
+
 // LIST OF ALL 
 $("#btnShowData").click(function getAllCountries() {
     $.ajax({
