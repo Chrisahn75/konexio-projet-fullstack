2356444632ebc673f6205ff7154d7d419db4b7cb
Chrisahn75 <chris.ahona@gmail.com>
2022-01-20 15:38:01 +0100

    try to put continent with selector

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// FORM PART COUNTRY
+// FORM PART COUNTRY AND CAPITAL
 $("#btnShowData").click(function () {
     let country = $("input").val();
-    let search = $("input[id=searchCountry]:checked", "#searchPart").val()
-    let searchC = $("input[id=searchCapital]:checked", "#searchPart").val()
+    let search = $("input[id=search]:checked", "#searchPart").val()
 
     if (country) {
         if (search === "country" || !search) {
@@ -10,35 +9,35 @@ $("#btnShowData").click(function () {
                 url: `https://restcountries.com/v3.1/name/${country}`,
                 success: function (data) {
                     data.forEach((country) => {
-                        $("#list").append(`<li> Country: ${country.name.common}</li>`);
-                        $("#list").append(`<li> Capital: ${country.capital}</li>`);
-                        $("#list").append(`<li> Continent: ${country.continents}</li>`);
+                        $("#list").append(`<li> Country : ${country.name.common}</li>`);
+                        $("#list").append(`<li> Capital : ${country.capital}</li>`);
+                        $("#list").append(`<li> Continent : ${country.continents}</li>`);
 
                         for(currency in country.currencies)  {
-                            $("#list").append(`<li> Monnaie : ${country.currencies[currency].name}</li>`);
+                            $("#list").append(`<li> Cash : ${country.currencies[currency].name}</li>`);
                         }
 
                         for(language in country.languages)  {
-                            $("#list").append(`<li> Langue : ${country.languages[language]}</li>`);
+                            $("#list").append(`<li> Language : ${country.languages[language]}</li>`);
                         }
                     })
                 },
              });
-        } else if (searchC === "capital"){
+        } else if (search === "capital"){
             $.ajax({
                 url: `https://restcountries.com/v3.1/capital/${country}`,
                 success: function (data) {
                     data.forEach((country) => {
-                        $("#list").append(`<li> Country: ${country.name.common}</li>`);
-                        $("#list").append(`<li> Capital: ${country.capital}</li>`);
-                        $("#list").append(`<li> Continent: ${country.continents}</li>`);
+                        $("#list").append(`<li> Country : ${country.name.common}</li>`);
+                        $("#list").append(`<li> Capital : ${country.capital}</li>`);
+                        $("#list").append(`<li> Continent : ${country.continents}</li>`);
 
                         for(currency in country.currencies)  {
-                            $("#list").append(`<li> Monnaie : ${country.currencies[currency].name}</li>`);
+                            $("#list").append(`<li> Cash : ${country.currencies[currency].name}</li>`);
                         }
 
                         for(language in country.languages)  {
-                            $("#list").append(`<li> Langue : ${country.languages[language]}</li>`);
+                            $("#list").append(`<li> Language : ${country.languages[language]}</li>`);
                         }
                     })
                 },
@@ -49,28 +48,46 @@ $("#btnShowData").click(function () {
     }
 });
 
-// LIST OF ALL 
-$("#btnShowData").click(function getAllCountries() {
+// SELECT CONTINENTS PART 
+
+$("select").change(function () {
+    select = $("select [id=continents]").val();
+  
     $.ajax({
-        url: "https://restcountries.com/v3.1/all",
-        success: function (data) {
-            data.forEach((country) => {
-                $("#list").append(`<h3>${country.name.common}</h3>`);
-                $("#list").append(`<li> Capitale : ${country.capital}</li>`);
-                $("#list").append(`<li> Continent: ${country.continents}</li>`);
+      url: `https://restcountries.com/v3.1/continents/${select}`,
+      success: function (data) {
+        data.forEach((country) => {
+          $("#list").append(`<li> Country: ${country.name.common} </li>`);
+          $("#list").append(`<li> Capital : ${country.capital}</li>`);
+          $("#list").append(`<li> Continent : ${country.continents}</li>`);
+        });
+      },
+    });
+  });
 
-                for(currency in country.currencies)  {
-                    $("#list").append(`<li> Monnaie : ${country.currencies[currency].name}</li>`);
-                }
+// LIST OF ALL 
 
-                for(language in country.languages)  {
-                    $("#list").append(`<li> Langue : ${country.languages[language]}</li>`);
-                }
-            })   
-        }
-    })
-});
+// $("#btnShowData").click(function getAllCountries() {
+//     $.ajax({
+//         url: "https://restcountries.com/v3.1/all",
+//         success: function (data) {
+//             data.forEach((country) => {
+//                 $("#list").append(`<h3>${country.name.common}</h3>`);
+//                 $("#list").append(`<li> Capitale : ${country.capital}</li>`);
+//                 $("#list").append(`<li> Continent : ${country.continents}</li>`);
 
-getAllCountries();
+//                 for(currency in country.currencies)  {
+//                     $("#list").append(`<li> Cash : ${country.currencies[currency].name}</li>`);
+//                 }
+
+//                 for(language in country.languages)  {
+//                     $("#list").append(`<li> Language : ${country.languages[language]}</li>`);
+//                 }
+//             })   
+//         }
+//     })
+// });
+
+// getAllCountries();
 
 
